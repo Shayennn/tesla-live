4799c79a85b6b418951dca345c51245bd9bf4d7a
Phitchawat L <me@phitchaw.at>
2024-01-26 16:10:07 +0700

    chore: show placeholder when can

--- main.go
+++ main.go
@@ -87,59 +87,54 @@ func handleLiveRequest(w http.ResponseWriter, r *http.Request) {
 
 	fileList := resp.Contents
 
-	if len(fileList) > 0 {
-		log.Printf("Found %d files", len(fileList))
-		re := regexp.MustCompile(`(\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2})`)
+	log.Printf("Found %d files", len(fileList))
+	re := regexp.MustCompile(`(\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2})`)
 
-		sort.SliceStable(fileList, func(i, j int) bool {
-			firstMatch := re.FindStringSubmatch(*fileList[i].Key)[0]
-			secondMatch := re.FindStringSubmatch(*fileList[j].Key)[0]
+	sort.SliceStable(fileList, func(i, j int) bool {
+		firstMatch := re.FindStringSubmatch(*fileList[i].Key)[0]
+		secondMatch := re.FindStringSubmatch(*fileList[j].Key)[0]
 
-			return strings.Compare(secondMatch, firstMatch) < 0
-		})
+		return strings.Compare(secondMatch, firstMatch) < 0
+	})
 
-		fileCount := len(fileList)
-		if fileCount > 8 {
-			fileCount = 8
+	fileCount := len(fileList)
+	if fileCount > 8 {
+		fileCount = 8
+	}
+
+	latestFiles := make(map[string]*s3.Object)
+
+	for _, file := range fileList[:fileCount] {
+		if strings.Contains(*file.Key, "front") && latestFiles["front"] == nil {
+			latestFiles["front"] = file
+		} else if strings.Contains(*file.Key, "back") && latestFiles["back"] == nil {
+			latestFiles["back"] = file
+		} else if strings.Contains(*file.Key, "left") && latestFiles["left"] == nil {
+			latestFiles["left"] = file
+		} else if strings.Contains(*file.Key, "right") && latestFiles["right"] == nil {
+			latestFiles["right"] = file
 		}
+	}
 
-		latestFiles := make(map[string]*s3.Object)
+	var keyToSign *string
 
-		for _, file := range fileList[:fileCount] {
-			if strings.Contains(*file.Key, "front") && latestFiles["front"] == nil {
-				latestFiles["front"] = file
-			} else if strings.Contains(*file.Key, "back") && latestFiles["back"] == nil {
-				latestFiles["back"] = file
-			} else if strings.Contains(*file.Key, "left") && latestFiles["left"] == nil {
-				latestFiles["left"] = file
-			} else if strings.Contains(*file.Key, "right") && latestFiles["right"] == nil {
-				latestFiles["right"] = file
-			}
-		}
-
-		var keyToSign *string
-
-		if latestFiles[camera] == nil {
-			keyToSign = aws.String("/placeholder.mp4")
-		} else {
-			keyToSign = latestFiles[camera].Key
-		}
-
-		req, _ := svc.GetObjectRequest(&s3.GetObjectInput{
-			Bucket: aws.String(bucketName),
-			Key:    keyToSign,
-		})
-
-		urlStr, err := req.Presign(5 * time.Second)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		http.Redirect(w, r, urlStr, http.StatusFound)
-		return
+	if latestFiles[camera] == nil {
+		keyToSign = aws.String("/placeholder.mp4")
 	} else {
-		http.Error(w, "No files found", http.StatusInternalServerError)
+		keyToSign = latestFiles[camera].Key
+	}
+
+	req, _ := svc.GetObjectRequest(&s3.GetObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    keyToSign,
+	})
+
+	urlStr, err := req.Presign(5 * time.Second)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+
+	http.Redirect(w, r, urlStr, http.StatusFound)
+	return
 }
